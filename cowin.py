535b9a94cc37abbda05e53c8f355582a4080550d
Priyanshu Dubey <37262690+priyanshudubey@users.noreply.github.com>
2021-05-29 15:01:43 +0530

    Update cowin.py

--- cowin.py
+++ cowin.py
@@ -18,7 +18,7 @@ def fetch_data():
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     final_url = base_url+qp
     response = requests.get(final_url, headers = headers)
-    #print(response.text)
+    print(response.text)
     extract_availability(response)
     
 def extract_availability(response):
